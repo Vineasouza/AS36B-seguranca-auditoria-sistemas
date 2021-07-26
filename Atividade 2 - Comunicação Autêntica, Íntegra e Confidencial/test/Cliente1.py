28a49789f0ba0460c75e96e22837ab1e0f5573de
Vineasouza <vineasouza33@hotmail.com>
2021-07-26 12:15:45 -0300

    Add: refatorando codigo da atividade 2 e comentando as etapas

--- Atividade 2 - Comunicação Autêntica, Íntegra e Confidencial/test/Cliente1.py
+++ Atividade 2 - Comunicação Autêntica, Íntegra e Confidencial/test/Cliente1.py
@@ -14,12 +14,16 @@ import hashlib
 import rsa
 import os.path
 
-#Código para gerar chaves pública e privada
+# Gera as chaves publica e privadas, retornando uma tupla (pub, priv)
+# pub(n,e) | priv(n,e,d,p,q)
+# n = valor de n = p*q, resultado da multiplicação de dois números primos grandes
+# e = valor do expoente comum da chave => valor padrão: 65537
+# d = valor da chave privada 
 (pubkey1,privkey1) = rsa.newkeys(1024)
-n = pubkey1.n
-e = pubkey1.e
-d = privkey1.d
-np = privkey1.n
+npublico_client1 = pubkey1.n
+epublico_client1 = pubkey1.e
+dprivado_client1 = privkey1.d
+nprivado_client1 = privkey1.n
 pubkey2 = None
 
 SOCKET_LIST = []
@@ -34,34 +38,43 @@ class Server(threading.Thread):
         lis.append(self.receive)
         read, write, err = select.select(lis, [], [])
         global pubkey2
-        #recebe n
+        
+        # Loop de recebimento da chave publica do cliente 2
+        # Armazena o n da chave publica do cliente 2 na variável npublico_client2
         for item in read: 
             try:
                 s = item.recv(1024)
                 if s != '':
-                    n2 = int(s)
+                    npublico_client2 = int(s)
                 else:
                     break
             except:
                 traceback.print_exc(file=sys.stdout)
                 break
-        #recebe e
+        
+        # Loop de recebimento da chave publica do cliente 2
+        # Armazena o e da chave publica do cliente 2 na variável epublico_client2
+        # Gera a chave publica do cliente 2 a partir do 'n' e do 'e' recebidos
         for item in read: 
             try:
                 s = item.recv(1024)
                 if s != '':
-                    e2 = int(s)
-                    pubkey2 = rsa.key.PublicKey(n2,e2)
+                    epublico_client2 = int(s)
+                    pubkey2 = rsa.key.PublicKey(npublico_client2,epublico_client2)
                 else:
                     break
             except:
                 traceback.print_exc(file=sys.stdout)
                 break
-        #recebe o ok do outro cliente para finalizar o handshake
+        
+        # Após o recebimento dos valores de 'n' e 'e' da chave publica do cliente 2,
+        # recebe um ok do cliente 2, sendo ele gravado no arquivo 'ok1.txt' 
+        # para posterior finalização do handshake entre os clientes
         for item in read:
             try:
                 s = item.recv(1024)
                 if s != '':
+                    # Le o valor 'Ok' e grava no arquivo 'ok1.txt'
                     ok = s
                     k = open('ok1.txt','wb')
                     k.write(ok)
@@ -71,37 +84,47 @@ class Server(threading.Thread):
             except:
                 traceback.print_exc(file=sys.stdout)
                 break
-        #loop para recebimento de mensagens
+
+        # Loop de recebimento das mensagens e da assinatura do cliente 2
         while 1:
-            #recebe a mensagem e descriptografa
+            # 1 - recebe a mensagem criptografada
+            # 2 - abre e le o arquivo onde a chave simetrica do cliente foi gravada e aramazena na variavel chave_simetrica_client1
+            # 3 - utiliza a chave simétrica para descriptografar a mensagem
             for item in read:
                 try:
                     s = item.recv(1024)
                     if s != '':
                         msgcriptografada = s
-                        #abrindo a chave simétrica através de um arquivo
-                        simk = open('keysimetrica.key','rb')
-                        keysimetrica = simk.read()
-                        f = Fernet(keysimetrica)
+                        arq_chave_simetrica = open('keysimetrica.key','rb')
+                        chave_simetrica_client1 = arq_chave_simetrica.read()
+                        f = Fernet(chave_simetrica_client1)
                         msg = f.decrypt(msgcriptografada).decode()    
                     else:
                         break
                 except:
                     traceback.print_exc(file=sys.stdout)
                     break
-            #recebe a assinatura, confere e printa a mensagem descriptografada na tela
+            
+            # 1 - recebe a assinatura da mensagem
+            # 2 - realiza a função digest (representação numérica de tamanho fixo do conteúdo de uma mensagem, 
+            # calculada por uma função hash) da assinatura recebida e armazena na variavel 'hash_assinatura_client2'
+            # 3 - realiza a verificação se a assinatura é valida 
+            #   3.1 - realiza o calculo (assinatura^e) % n (sendo 'e' e 'n' recebidos anteriormente) e armazena na variavel 'assinatura_client2'
+            #   3.2 - compara hashnovo com assi2novo para validar a assinatura
+            #       3.2.1 - Se valida, imprime a mensagem
+            #       3.2.2 - Se inválida, imprime um aviso  
             for item in read:
                 try:
                     s = item.recv(1024)
                     if s != '':
-                        assi2 = s
+                        assinatura_client2 = s
                         msgh = msg.encode()
-                        hashnovo = int.from_bytes(hashlib.sha1(msgh).digest(), byteorder='big')
-                        assi2novo = pow(int(assi2),e2,n2)
-                        if(hashnovo == assi2novo):
-                            print(msg + '\n:')
+                        hash_assinatura_client2 = int.from_bytes(hashlib.sha1(msgh).digest(), byteorder='big')
+                        assinatura_nova_client2 = pow(int(assinatura_client2),epublico_client2,npublico_client2)
+                        if(hash_assinatura_client2 == assinatura_nova_client2):
+                            print(msg + '\n>> ')
                         else:
-                            print('A mensagem não foi recebida de forma íntegra!')
+                            print('>> A mensagem não foi recebida de forma íntegra! <<')
                             exit()
                 except:
                     traceback.print_exc(file=sys.stdout)
@@ -115,36 +138,41 @@ class Client(threading.Thread):
 
     def client(self, host, port, msg):
         sent = self.sock.send(msg)
-        # print "Sent\n"
 
     def run(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         try:
-            host = input("Enter the server IP:\n")
-            port = int(input("Enter the server Destination Port:\n"))
+            host = input("Enter the server IP\n>> ")
+            port = int(input("EEnter the server Destination Port\n>> "))
         except EOFError:
             print("Error")
             return 1
-        print("Connecting\n")
+        print(">> Connecting <<\n")
         s = ''
         self.connect(host, port)
-        print("Connected\n")
-        user_name = input("Enter the User Name to be Used:\n")
+        print(">> Connected << \n")
+        user_name = input("Enter the User Name to be Used\n>> ")
         time.sleep(1)
         srv = Server()
         srv.initialise(self.sock)
         srv.daemon = True
-        print("Starting service")
+        print(">> Starting service << ")
         srv.start()
         SOCKET_LIST.append(self.sock)
-        #envia n da chave pública 1
-        self.client(host,port,bytes(str(n), encoding='utf8'))
+
+
+        # Ao iniciar o cliente, realiza o envio do valor de 'n' da chave publica do cliente1 para o cliente 2
+        self.client(host,port,bytes(str(npublico_client1), encoding='utf8'))
         time.sleep(1)
-        #envia e da chave pública 1 e aguarda pela chave pública 2
-        self.client(host,port,bytes(str(e), encoding='utf8'))
+
+
+        # Realiza o envio do valor de 'e' da chave publica do cliente 1  e fica aguardando o recebimento da chave publica do cliente 2
+        self.client(host,port,bytes(str(epublico_client1), encoding='utf8'))
         time.sleep(3)
-        #cria chave simétrica e a envia criptografada
+        
+        # Gera uma chave simétrica, armazenando-a no arquivo 'keyssimetrica.key' e 
+        # realiza o envio desta chave , criptografando-a  a partir da chave publica do cliente 2
         keysimetrica = Fernet.generate_key()
         f = Fernet(keysimetrica)
         sk = open('keysimetrica.key','wb') #salva em um arquivo
@@ -153,23 +181,33 @@ class Client(threading.Thread):
         simkeycripto = rsa.encrypt(keysimetrica,pubkey2)
         self.client(host,port,simkeycripto)
         time.sleep(4)
-        #cria o hash e a assinatura da chave e o envia
+
+        # cria o hash e a assinatura da chave e o envia
+        # Gera o hash utilizando o algoritmo SHA-1 a partir da chave simétrica
+        # gerando assim a assinatura do cliente e realiza o envio para o cliente 2
         hash1 = rsa.compute_hash(keysimetrica, 'SHA-1') 
         assinatura = rsa.sign_hash(hash1, privkey1, 'SHA-1')
         self.client(host,port,assinatura)
         time.sleep(5)
+
+        # Loop de verificação do handshake
+        # Verifica se o arquivo 'ok1.txt' ja existe e se existe algo gravado nele
+        # Se sim, passa para o próximo passo, se não, continua aguardando o fim do handshake, com um intervalo de 10s
         while 1: #checa se já acabou o handshake
             if (os.path.isfile('ok1.txt')): 
                 if (os.path.getsize('ok1.txt') > 0):
                     break
                 else:
-                    print("Aguardando Fim do Handshake...")
+                    print(">> Aguardando Fim do Handshake... <<")
                     time.sleep(10)
             else:
-                print("Aguardando Fim do Handshake...")
+                print(">> Aguardando Fim do Handshake... <<")
                 time.sleep(10)
+        
+        # Loop de envio da mensagem, sendo ela delimitada a 255 caracteres
+        # Para o envio da mensagem:      
         while 1:
-            msg = input(':')
+            msg = input('>> ')
             if(len(msg)>256): #verificando se a mensagem possui mais de 255 caracteres
                 print("Não é permitido enviar mensagem com mais de 255 caracteres!")
                 break
@@ -177,19 +215,26 @@ class Client(threading.Thread):
                 break
             if msg == '':
                 continue
-            #para enviar mensagem
+            # 1 - concatenado o valor de input de msg com o user_name definido e realizado o encode da msg
             msg = user_name + ":" + msg
             msgh = msg.encode()
+
+            # 2 - realizada a criptografia da mensagem utlizando fernet e **envia para o client 2**
             msgcriptografada = f.encrypt(msg.encode())
             self.client(host, port, msgcriptografada)
             time.sleep(2)
-            hash2= int.from_bytes(hashlib.sha1(msgh).digest(), byteorder='big') #hash e assinatura da mensagem
-            assinatura2 = pow(hash2,d,np)
+
+            # 3 - gerado a hash da mensagem utilizando o algoritmo SHA-1
+            hash2= int.from_bytes(hashlib.sha1(msgh).digest(), byteorder='big') 
+            
+            # 4 - gerando a assinatura a partir desta hash e dos valores de 'd' e 'n' do cliente 1 e **envia para o client 2**
+            # realiza o calculo (hash^d) % n
+            assinatura2 = pow(hash2,dprivado_client1,nprivado_client1)
             self.client(host,port,bytes(str(assinatura2), encoding='utf8'))
             time.sleep(5)
         return (1)
 
 if __name__ == '__main__':
-    print("Starting client")
+    print(">> Starting client <<")
     cli = Client()
     cli.start()
--- Atividade 2 - Comunicação Autêntica, Íntegra e Confidencial/test/Cliente2.py
+++ Atividade 2 - Comunicação Autêntica, Íntegra e Confidencial/test/Cliente2.py
@@ -14,12 +14,18 @@ import hashlib
 import rsa
 import os.path
 
-#Código para gerar chaves pública e privada
+# Gera as chaves publica e privadas, retornando uma tupla (pub, priv)
+# pub(n,e) | priv(n,e,d,p,q)
+# n = valor de n = p*q, resultado da multiplicação de dois números primos grandes
+# e = valor do expoente comum da chave => valor padrão: 65537
+# d = valor da chave privada 
 (pubkey2,privkey2) = rsa.newkeys(1024)
-n = pubkey2.n
-e = pubkey2.e
-d = privkey2.d
-np = privkey2.n
+npublico_cliente2 = pubkey2.n
+epublico_cliente2 = pubkey2.e
+dprivado_client2 = privkey2.d
+nprivado_client2 = privkey2.n
+
+pubkey1 = None
 
 class Server(threading.Thread):
     
@@ -30,30 +36,38 @@ class Server(threading.Thread):
         lis = []
         lis.append(self.receive)
         read, write, err = select.select(lis, [], [])
-        #recebe n
+        global pubkey1
+
+        # Loop de recebimento da chave publica do cliente 1
+        # Armazena o n da chave publica do cliente 1 na variável npublico_client1
         for item in read: 
             try:
                 s = item.recv(1024)
                 if s != '':
-                    n1 = int(s)
+                    npublico_client1 = int(s)
                 else:
                     break
             except:
                 traceback.print_exc(file=sys.stdout)
                 break
-        #recebe e
+        
+        # Loop de recebimento da chave publica do cliente 1
+        # Armazena o e da chave publica do cliente 1 na variável epublico_client1
+        # Gera a chave publica do cliente 1 a partir do 'n' e do 'e' recebidos
         for item in read: 
             try:
                 s = item.recv(1024)
                 if s != '':
-                    e1 = int(s)
-                    pubkey1 = rsa.key.PublicKey(n1,e1)
+                    epublico_client1 = int(s)
+                    pubkey1 = rsa.key.PublicKey(npublico_client1,epublico_client1)
                 else:
                     break
             except:
                 traceback.print_exc(file=sys.stdout)
                 break
-        #recebe chave simétrica
+        
+        # recebe a chave simétrica do cliente 1  cirptografada 
+        # descriptografa a chave a partir da chave publica do cliente 2
         for item in read:
             try:
                 s = item.recv(1024)
@@ -65,14 +79,18 @@ class Server(threading.Thread):
             except:
                 traceback.print_exc(file=sys.stdout)
                 break
+        
         #recebe e confere a assinatura da chave simétrica
+        # recebe a assinatura do cliente 1 
+        # verifica se a assinatura corresponde a chave simétrica recebida, a partir da chave assimetrica publica do cliente 1   
+        # confirma o recebimento da chave simetrica do cliente 1
         for item in read:
             try:
                 s = item.recv(1024)
                 if s != '':
                     assi = s
                     if (rsa.verify(keysimetrica, assi, pubkey1)):
-                        print('A chave simétrica foi recebida com Sucesso')
+                        print('>> A chave simétrica foi recebida com sucesso << ')
                         sk = open('keysimetrica2.key','wb') #salva em um arquivo
                         sk.write(keysimetrica)                                                                                                      
                         sk.close()
@@ -81,14 +99,16 @@ class Server(threading.Thread):
                         ok = "ok" 
                         self.receive.send(bytes(ok, encoding='utf8'))
                     else:
-                        print('A chave simétrica não foi recebida com Sucesso!')
+                        print('>> A chave simétrica não foi recebida com sucesso! <<')
                         exit()
             except:
                 traceback.print_exc(file=sys.stdout)
                 break
-        #loop para recebimento de mensagens
+        
+        # Loop de recebimento das mensagens  e da assinatura do cliente 1
         while 1:
-            #recebe a mensagem e descriptografa
+            # 1 - recebe a mensagem criptografada
+            # 2- utiliza a chave simétrica do cliente 1 recebida para descriptografar a mensagem
             for item in read:
                 try:
                     s = item.recv(1024)
@@ -100,19 +120,27 @@ class Server(threading.Thread):
                 except:
                     traceback.print_exc(file=sys.stdout)
                     break
-            #recebe a assinatura, confere e printa a mensagem descriptografada na tela
+            
+            # 1- recebe a assinatura da mensagem
+            # 2 - realiza a função digest (representação numérica de tamanho fixo do conteúdo de uma mensagem, 
+            # calculada por uma função hash) da assinatura recebida e armazena na variavel 'hash_assinatura_client1'
+            # 3 - realiza a verificação se a assinatura é valida 
+            #   3.1 - realiza o calculo (assinatura^e) % n (sendo 'e' e 'n' recebidos anteriormente) e armazena na variavel 'assinatura_client2'
+            #   3.2 - compara hashnovo com assi2novo para validar a assinatura
+            #       3.2.1 - Se valida, imprime a mensagem
+            #       3.2.2 - Se inválida, imprime um aviso  
             for item in read:
                 try:
                     s = item.recv(1024)
                     if s != '':
-                        assi2 = s
+                        assinatura_client1 = s
                         msgh = msg.encode()
-                        hashnovo = int.from_bytes(hashlib.sha1(msgh).digest(), byteorder='big')
-                        assi2novo = pow(int(assi2),e1,n1)
-                        if(hashnovo == assi2novo):
-                            print(msg + '\n:')
+                        hash_assinatura_client1 = int.from_bytes(hashlib.sha1(msgh).digest(), byteorder='big')
+                        assinatura_nova_client1 = pow(int(assinatura_client1),epublico_client1,npublico_client1)
+                        if(hash_assinatura_client1 == assinatura_nova_client1):
+                            print(msg + '\n>> ')
                         else:
-                            print('A mensagem não foi recebida de forma íntegra!')
+                            print('>> A mensagem não foi recebida de forma íntegra! <<')
                             exit()
                 except:
                     traceback.print_exc(file=sys.stdout)
@@ -125,34 +153,39 @@ class Client(threading.Thread):
 
     def client(self, host, port, msg):
         sent = self.sock.send(msg)
-        # print "Sent\n"
 
     def run(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         try:
-            host = input("Enter the server IP:\n")
-            port = int(input("Enter the server Destination Port:\n"))
+            host = input("Enter the server IP\n>> ")
+            port = int(input("EEnter the server Destination Port\n>> "))
         except EOFError:
             print("Error")
             return 1
-        print("Connecting\n")
+        print(">> Connecting <<\n")
         s = ''
         self.connect(host, port)
-        print("Connected\n")
+        print(">> Connected << \n")
         user_name = input("Enter the User Name to be Used:\n")
         time.sleep(1)
         srv = Server()
         srv.initialise(self.sock)
         srv.daemon = True
-        print("Starting service")
+        print(">> Starting service << ")
         srv.start()
-        #envia n da chave pública 2
-        self.client(host,port,bytes(str(n), encoding='utf8'))
+
+        # Ao iniciar o cliente, realiza o envio do valor de 'n' da chave publica do cliente 2 para o cliente 1
+        self.client(host,port,bytes(str(npublico_cliente2), encoding='utf8'))
         time.sleep(1)
-        #envia e da chave pública 2 e aguarda pela chave pública 2
-        self.client(host,port,bytes(str(e), encoding='utf8'))
+
+         # Realiza o envio do valor de 'e' da chave publica do cliente 2  e fica aguardando o recebimento da chave publica do cliente 1
+        self.client(host,port,bytes(str(epublico_cliente2), encoding='utf8'))
         time.sleep(5)
+
+        # Loop de verificação do handshake
+        # Verifica se o arquivo 'keychavesimetrica2.key' ja existe e se exite algo gravado nele
+        # Se sim, passa para o próximo passo, se não, continua aguardando o fim do handshake, com um intervalo de 10s
         while 1: #checa se já recebeu a chave simétrica
             if (os.path.isfile('keysimetrica2.key')): 
                 if (os.path.getsize('keysimetrica2.key') > 0):
@@ -163,8 +196,15 @@ class Client(threading.Thread):
             else:
                 print("Aguardando Fim do Handshake...")
                 time.sleep(10)
+        
+        # Loop de envio da mensagem, sendo ela delimitada a 255 caracteres
+        # Para o envio da mensagem:
+        # 1 - concatenado o valor de input de msg com o user_name definido e realizado o encode da msg
+        
+        # 4 - gerado a hash da mensagem utilizando o algoritmo SHA-1
+        
         while 1:
-            msg = input(':')
+            msg = input('>> ')
             if(len(msg)>256): #verificando se a mensagem possui mais de 255 caracteres
                 print("Não é permitido enviar mensagem com mais de 255 caracteres!")
                 break
@@ -172,23 +212,31 @@ class Client(threading.Thread):
                 break
             if msg == '':
                 continue
-            #para enviar mensagem
+            # 1 - concatenado o valor de input de msg com o user_name definido e realizado o encode da msg
             msg = user_name + ":" + msg
             msgh = msg.encode()
+
             #pega a chave simétrica de um arquivo
+            # 2 - Abre e le o arquivo onde a chave simétrica foi armazenada e atribui ao Fernet
             simk = open('keysimetrica2.key','rb')
             keysimetrica = simk.read()
             f = Fernet(keysimetrica)
+
+            # 3 - realiza a criptografia a partir da chave simétrica da mensagem utlizando fernet e **envia para o client 1**
             msgcriptografada = f.encrypt(msg.encode())
             self.client(host, port, msgcriptografada)
             time.sleep(2)
-            hash1 = int.from_bytes(hashlib.sha1(msgh).digest(), byteorder='big') #hash e assinatura da mensagem
-            assinatura1 = pow(hash1,d,np)
+            # 4 - gerado a hash da mensagem utilizando o algoritmo SHA-1
+            hash1 = int.from_bytes(hashlib.sha1(msgh).digest(), byteorder='big') 
+            
+            # 5 - gerando a assinatura a partir desta hash e dos valores de 'd' e 'n' do cliente 2 e **envia para o client 1**
+             # realiza o calculo (hash^d) % n
+            assinatura1 = pow(hash1,dprivado_client2,nprivado_client2)
             self.client(host,port,bytes(str(assinatura1), encoding='utf8'))
             time.sleep(5)
         return (1)
 
 if __name__ == '__main__':
-    print("Starting client")
+    print(">> Starting client <<")
     cli = Client()
     cli.start()
